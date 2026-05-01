6302a81693fe881a1de03ca3a41804ac125fd888
Luis Enrique Vazquez Bautista <quiquesixx@gmail.com>
2026-05-01 13:58:10 -0600

    Remove node_modules and dist from tracking

--- dist/interfaces/BuscarOpciones.d.ts
+++
@@ -1,7 +0,0 @@
-export interface BuscarOpciones {
-    titulo?: string;
-    autor?: string;
-    anioPublicacion?: number;
-    prestado?: boolean;
-}
-//# sourceMappingURL=BuscarOpciones.d.ts.map
--- dist/interfaces/BuscarOpciones.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=BuscarOpciones.js.map
