0b29f149e6a52dcd660b822ba1c3fd55b1195a25
Luis Enrique Vazquez Bautista <quiquesixx@gmail.com>
2026-04-30 23:06:30 -0600

    proyectoBiblioteca

--- dist/interfaces/IPublicacion.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=IPublicacion.js.map
--- dist/utils/buscarPorProp.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function buscarPorProp<T, K extends keyof T>(lista: T[], prop: K, valor: T[K]): T | undefined;
-//# sourceMappingURL=buscarPorProp.d.ts.map
