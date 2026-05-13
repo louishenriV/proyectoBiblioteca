20fa7d4600feb81c5291104663a95abe5f041cd1
Luis Enrique Vazquez Bautista <quiquesixx@gmail.com>
2026-05-12 23:22:24 -0600

    Reemplazar la lógica JSON por Prisma + PostgreSQL

--- src/services/bibliotecaService.ts
+++ src/services/bibliotecaService.ts
@@ -1,43 +1,12 @@
-import { error } from "console";
-import { Biblioteca } from "../Biblioteca.js";
-import { Libro } from "../models/Libro.js";
-import { readFile, writeFile } from "fs/promises"; //leer y escribir archivos de forma async
-//ya no depende de la memoria, ahora usamos el almacenamiento
 
-const ruta = "./data/libros.json";//mini BD donde se guardan los datos
-const biblioteca = new Biblioteca<Libro>("API Biblioteca");//contenedor en memoria
+import prisma from "../prismaClient";
+//importamos la instancia de Prisma Client para interactuar con la base de datos
 
-export const cargarLibros = async () => {
-    try{ //lee el archivo libros.json...
-        const data = await readFile(ruta, "utf8");//...y espera
-        const libros = JSON.parse(data)//convierte el texto en JSON
-
-        biblioteca.coleccion = libros.map((obj: any) =>
-            new Libro( //creación de objeto, ya no son JSONs planos
-                obj.titulo,
-                obj.autor,
-                obj.anioPublicacion,
-                obj.prestado
-            )
-        // esto hace que JSON -> objetos simples -> instancias de clase
-        );
-
-    }catch(error){ //manejo basico de errores
-        console.error("Error cargando libros", error)
-    }
-};
-
-//Guardar en archivo, convierte el objeto en texto JSON
-const guardarLibros = async () => {
-    const json = JSON.stringify(biblioteca.coleccion, null, 2);//identacion
-    await writeFile(ruta, json, "utf8");//guarda el archivo
-};
 
 //Obtener todos los libros
-export const obtenerLibros = () => {
-    return biblioteca.coleccion; //devuelve los datos en memoria
-    /*console.log("coleccion REAL:", biblioteca.coleccion);
-    return biblioteca.coleccion;**/
+export const obtenerLibros = async () => {
+    return await prisma.libro.findMany();
+  //conectarse a PostgreSQL, hacer la consulta, esperar la respuesta y traerla de vuelta
 };
 
 //agregar un libro
@@ -45,22 +14,35 @@ export const agregarLibro = async (data:any) => { //recibe datos desde la API
     const {titulo, autor, anioPublicacion, prestado} = data;
     //recibimos los datos del cliente y se extraen con destructuring en un JSON
     
-    const nuevoLibro = new Libro(titulo, autor, anioPublicacion, prestado);
-    biblioteca.agregar(nuevoLibro); //lo agregamos a la colección
-
-    await guardarLibros(); 
+    const nuevoLibro = await prisma.libro.create({
+        data: {
+            titulo,
+            autor,
+            anioPublicacion,
+            prestado: prestado ?? false
+        }
+    })
 
     return nuevoLibro;
 };
 
 //eliminar un libro
-export const eliminarLibro = (id: string): void => {
-    biblioteca.eliminar(id)
-    guardarLibros()
+export const eliminarLibro = async (id:string) => {
+    await prisma.libro.delete({
+        where: { id }
+    })
 };
 
 //actualizar libro 
-export const actualizarLibro = (id: string): void => {
-    biblioteca.actualizar(id)
-    guardarLibros()
+export const actualizarLibro = async (id: string) => {
+    const libroEncontrado = await prisma.libro.findUnique({
+        where: { id }
+    })
+    if (!libroEncontrado) return;
+    libroEncontrado.prestado = !libroEncontrado.prestado
+    
+    await prisma.libro.update({
+        where : { id }, 
+        data: { prestado: libroEncontrado.prestado }
+    })
 }
